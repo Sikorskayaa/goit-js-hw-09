dfb7b22a8e9ba89e765c4f6395cf53efa9694bd8
Sikorskayaa <133200518+Sikorskayaa@users.noreply.github.com>
2024-04-04 20:06:36 +0200

    second commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+stopBtn.disabled = true;
+let interval = null;
+
+startBtn.addEventListener('click', handleBtnStart);
+
+function handleBtnStart() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+
+  interval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+stopBtn.addEventListener('click', handleBtnStop);
+
+function handleBtnStop() {
+  clearInterval(interval);
+
+  stopBtn.disabled = true;
+  startBtn.disabled = false;
+}
